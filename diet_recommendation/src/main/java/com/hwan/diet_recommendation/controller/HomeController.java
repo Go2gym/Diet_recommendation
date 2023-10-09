847931dd99a72ef6f7317cd9aa5eacd783e6206c
BubblingSODA <go2gym365@gmail.com>
2023-10-09 01:18:40 +0000

    member controller without error view

--- diet_recommendation/src/main/java/com/hwan/diet_recommendation/controller/HomeController.java
+++ diet_recommendation/src/main/java/com/hwan/diet_recommendation/controller/HomeController.java
@@ -2,7 +2,6 @@ package com.hwan.diet_recommendation.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.SessionAttribute;
 
 
